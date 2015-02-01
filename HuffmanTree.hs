ac971979df85f26403f5fb4fbdde3e836dc62b39
Asmund Berge <asmundliseth@gmail.com>
2015-02-01 20:06:11 +0100

    First compiling version of huffmantree.hs

--- HuffmanTree.hs
+++ HuffmanTree.hs
@@ -1,39 +1,40 @@
 module HuffmanTree where
 
 import Data.Word
-import qualified Data.ByteString as Bs
+import Data.Map
+import qualified Data.List as L
+import Data.ByteString
+import Control.Monad
 
-data Tree = Node Tree Tree
+data Tree = Branch Tree Tree
           | Leaf Word8
           deriving(Eq, Show)
 
-data WeightTree = Tree Int
-    deriving(Eq, Show, Ord)
+data WeightTree = WeightTree Int Tree
+    deriving(Eq, Show)
 
--- count::Bs.ByteString->M.Map Word8 Int
--- count = Bs.foldl akk M.empty
---     where akk m c = if c `M.member` m
---         then M.insert c (m M.!c + 1) m
---         else M.insert c 1 m
+instance Ord WeightTree where
+    (WeightTree a _) <= (WeightTree b _) = a <= b
 
--- instance Ord HuffmanTree where
---     EmptyTree <= _ = True
---     _ <= EmptyTree = False
---     HuffmanNode n _ _ <= HuffmanNode n' _ _ = n <= n'
---     HuffmanNode n _ _ <= HuffmanLeaf n' _ = n <= n'
---     HuffmanLeaf n _ <= HuffmanNode n' _ _ = n <= n'
---     HuffmanLeaf n _ <= HuffmanLeaf n' _ = n <= n'
--- 
--- combine::HuffmanTree->HuffmanTree->HuffmanTree
--- combine t@(HuffmanNode n _ _) t'@(HuffmanNode n' _ _) = HuffmanNode (n+n') t t'
--- combine t@(HuffmanLeaf n _) t'@(HuffmanNode n' _ _) = HuffmanNode (n+n') t t'
--- combine t@(HuffmanNode n _ _) t'@(HuffmanLeaf n' _) = HuffmanNode (n+n') t t'
--- combine t@(HuffmanLeaf n _) t'@(HuffmanLeaf n' _) = HuffmanNode (n+n') t t'
--- 
--- leafList::M.Map Word8 Int->[HuffmanTree]
--- leafList = M.foldWithKey addLeaf []
---     where addLeaf k a ls = HuffmanLeaf a k : ls
--- 
--- buildTree::[HuffmanTree]->HuffmanTree
--- buildTree [t] = t
--- buildTree (t1:t2:ts) = buildTree (sort ((combine t1 t2):ts))
+get_weights :: ByteString -> [ WeightTree ]
+get_weights bytes = [ WeightTree  weight (Leaf byte) | (byte, weight) <- toList weightMap ]
+    where weightMap = fromListWith (+) [ (c, 1) | c <- unpack bytes ]
+
+build_tree :: [ WeightTree ] -> Tree
+build_tree leaves = build [] $ L.sort leaves
+    where combine (WeightTree wa a) (WeightTree wb b) = WeightTree (wa + wb) (Branch a b)
+          build [WeightTree _ t] [] = t 
+          build branches leaves = build ((combine small1 small2):branches'') leaves''
+              where smallest [] (b:bs) = (b, [], bs)
+                    smallest (a:as) [] = (a, as, [])
+                    smallest (a:as) (b:bs) | a < b     = (a, as, b:bs)
+                                           | otherwise = (b, a:as, bs)
+                    (small1, branches', leaves') = smallest branches leaves
+                    (small2, branches'', leaves'') = smallest branches' leaves'
+          
+get_bitstring :: Tree -> Word8 -> Maybe [Bool]
+get_bitstring (Leaf w) x | w == x = Just []
+                         | otherwise = Nothing
+get_bitstring (Branch a b) x = (liftM (False:) ( get_bitstring a x))
+                               `mplus`
+                               (liftM (True:) ( get_bitstring b x))
